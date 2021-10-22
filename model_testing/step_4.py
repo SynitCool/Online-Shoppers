8c13e6a1944e42fbecbb7d58d5952e156b3625d7
BASI <zolarpixel3@gmail.com>
2021-10-23 00:19:01 +0700

    edit step_4

---
+++ model_testing/step_4.py
@@ -0,0 +1,300 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from model_testing.config import DATASET_PATH
+from model_testing.config import X_CATEGORICAL_COLUMNS
+from model_testing.config import CATEGORICAL_COLUMNS
+from model_testing.config import Y_COLUMN
+from model_testing.config import MODEL
+from model_testing.config import MODEL_TITLE
+from model_testing.config import OBJECT_COLUMNS
+from model_testing.config import X_OBJECT_COLUMNS
+
+from cleaning.cleaning_data import read_data
+from cleaning.cleaning_data import onehot_data
+from cleaning.cleaning_data import encode_data
+
+from modelling.model_selection import train_test
+
+from evaluating.evaluate_model import plot_classification_report
+from evaluating.evaluate_model import plot_confusion_matrix
+
+
+def onehot_fe_categorical_columns():
+    """
+    Process testing
+
+    read data --> encode data --> feature engineering --> modelling --> evaluation
+    feature engineering
+    combining columns
+    [Administrative_Duration, Informational_Duration, ProductRelated_Duration] --> time spent
+    [BounceRates, ExitRates] --> dislike rates
+    [SpecialDay] --> y_n_special_day >= 0.5 = 1 and < 0.5 = 0
+    [Month] --> semester = [1, 2]
+
+    preprocessing
+    one hot categorical columns
+    """
+
+    sems_1 = ["Jan", "Feb", "Mar", "Apr", "May", "June"]
+    sems_2 = ["Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    duration_columns = [
+        "Administrative_Duration",
+        "Informational_Duration",
+        "ProductRelated_Duration",
+    ]
+    rates_columns = ["BounceRates", "ExitRates"]
+    new_cat_columns = ["y_n_special_day", "Semester"]
+    cat_columns = X_CATEGORICAL_COLUMNS.copy()
+    cat_columns.extend(new_cat_columns)
+
+    # Loading data
+    df = read_data(DATASET_PATH)
+
+    # Feature Engineering
+    df["TimeSpent"] = df[duration_columns].sum(axis=1)
+    df["DislikeRates"] = df[rates_columns].sum(axis=1)
+    df["y_n_special_day"] = df["SpecialDay"].apply(lambda x: 1 if x >= 0.5 else 0)
+    df["Semester"] = df["Month"].apply(lambda x: 1 if x in sems_1 else 2)
+
+    # Preprocessing
+    one_hot_df = onehot_data(df, cat_columns)
+
+    df = df.drop(columns=cat_columns)
+
+    df = pd.concat([df, one_hot_df], axis=1)
+
+    # Modelling
+    X = df.drop(columns=[Y_COLUMN])
+    y = df[Y_COLUMN]
+
+    X_train, X_test, y_train, y_test = train_test(X, y, MODEL)
+
+    for i in range(len(MODEL)):
+        print(f"\nTitle : {MODEL_TITLE[i]}")
+        plot_classification_report(y_test, MODEL[i].predict(X_test))
+
+    fig, ax = plt.subplots(figsize=(15, 9), nrows=2, ncols=2)
+
+    for i in range(4):
+        y_pred = MODEL[i].predict(X_test)
+        if i < 2:
+            plot_confusion_matrix(y_test, y_pred, ax=ax[0, i])
+
+            ax[0, i].set_title(MODEL_TITLE[i])
+        else:
+            i_ = i - 2
+
+            plot_confusion_matrix(y_test, y_pred, ax=ax[1, i_])
+
+            ax[1, i_].set_title(MODEL_TITLE[i])
+
+
+def encode_fe_categorical_columns():
+    """
+    Process testing
+
+    read data --> encode data --> feature engineering --> modelling --> evaluation
+    feature engineering
+    combining columns
+    [Administrative_Duration, Informational_Duration, ProductRelated_Duration] --> time spent
+    [BounceRates, ExitRates] --> dislike rates
+    [SpecialDay] --> y_n_special_day >= 0.5 = 1 and < 0.5 = 0
+    [Month] --> semester = [1, 2]
+
+    preprocessing
+    encode categorical columns
+    """
+
+    sems_1 = ["Jan", "Feb", "Mar", "Apr", "May", "June"]
+    sems_2 = ["Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    duration_columns = [
+        "Administrative_Duration",
+        "Informational_Duration",
+        "ProductRelated_Duration",
+    ]
+    rates_columns = ["BounceRates", "ExitRates"]
+    new_cat_columns = ["y_n_special_day", "Semester"]
+    cat_columns = CATEGORICAL_COLUMNS.copy()
+    cat_columns.extend(new_cat_columns)
+
+    # Loading data
+    df = read_data(DATASET_PATH)
+
+    # Feature Engineering
+    df["TimeSpent"] = df[duration_columns].sum(axis=1)
+    df["DislikeRates"] = df[rates_columns].sum(axis=1)
+    df["y_n_special_day"] = df["SpecialDay"].apply(lambda x: 1 if x >= 0.5 else 0)
+    df["Semester"] = df["Month"].apply(lambda x: 1 if x in sems_1 else 2)
+
+    # Preprocessing
+    encoded_df = encode_data(df, cat_columns)
+
+    df = df.drop(columns=cat_columns)
+
+    df = pd.concat([df, encoded_df], axis=1)
+
+    # Modelling
+    X = df.drop(columns=[Y_COLUMN])
+    y = df[Y_COLUMN]
+
+    X_train, X_test, y_train, y_test = train_test(X, y, MODEL)
+
+    for i in range(len(MODEL)):
+        print(f"\nTitle : {MODEL_TITLE[i]}")
+        plot_classification_report(y_test, MODEL[i].predict(X_test))
+
+    fig, ax = plt.subplots(figsize=(15, 9), nrows=2, ncols=2)
+
+    for i in range(4):
+        y_pred = MODEL[i].predict(X_test)
+        if i < 2:
+            plot_confusion_matrix(y_test, y_pred, ax=ax[0, i])
+
+            ax[0, i].set_title(MODEL_TITLE[i])
+        else:
+            i_ = i - 2
+
+            plot_confusion_matrix(y_test, y_pred, ax=ax[1, i_])
+
+            ax[1, i_].set_title(MODEL_TITLE[i])
+
+
+def onehot_fe_object_columns():
+    """
+    Process testing
+
+    read data --> encode data --> feature engineering --> modelling --> evaluation
+    feature engineering
+    combining columns
+    [Administrative_Duration, Informational_Duration, ProductRelated_Duration] --> time spent
+    [BounceRates, ExitRates] --> dislike rates
+    [SpecialDay] --> y_n_special_day >= 0.5 = 1 and < 0.5 = 0
+    [Month] --> semester = [1, 2]
+
+    preprocessing
+    one hot object columns
+    """
+
+    sems_1 = ["Jan", "Feb", "Mar", "Apr", "May", "June"]
+    sems_2 = ["Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    duration_columns = [
+        "Administrative_Duration",
+        "Informational_Duration",
+        "ProductRelated_Duration",
+    ]
+    rates_columns = ["BounceRates", "ExitRates"]
+    new_cat_columns = ["y_n_special_day", "Semester"]
+    obj_columns = X_OBJECT_COLUMNS.copy()
+    obj_columns.extend(new_cat_columns)
+
+    # Loading data
+    df = read_data(DATASET_PATH)
+
+    # Feature Engineering
+    df["TimeSpent"] = df[duration_columns].sum(axis=1)
+    df["DislikeRates"] = df[rates_columns].sum(axis=1)
+    df["y_n_special_day"] = df["SpecialDay"].apply(lambda x: 1 if x >= 0.5 else 0)
+    df["Semester"] = df["Month"].apply(lambda x: 1 if x in sems_1 else 2)
+
+    # Preprocessing
+    one_hot_df = onehot_data(df, obj_columns)
+
+    df = df.drop(columns=obj_columns)
+
+    df = pd.concat([df, one_hot_df], axis=1)
+
+    # Modelling
+    X = df.drop(columns=[Y_COLUMN])
+    y = df[Y_COLUMN]
+
+    X_train, X_test, y_train, y_test = train_test(X, y, MODEL)
+
+    for i in range(len(MODEL)):
+        print(f"\nTitle : {MODEL_TITLE[i]}")
+        plot_classification_report(y_test, MODEL[i].predict(X_test))
+
+    fig, ax = plt.subplots(figsize=(15, 9), nrows=2, ncols=2)
+
+    for i in range(4):
+        y_pred = MODEL[i].predict(X_test)
+        if i < 2:
+            plot_confusion_matrix(y_test, y_pred, ax=ax[0, i])
+
+            ax[0, i].set_title(MODEL_TITLE[i])
+        else:
+            i_ = i - 2
+
+            plot_confusion_matrix(y_test, y_pred, ax=ax[1, i_])
+
+            ax[1, i_].set_title(MODEL_TITLE[i])
+
+
+def encode_fe_object_columns():
+    """
+    Process testing
+
+    read data --> encode data --> feature engineering --> modelling --> evaluation
+    feature engineering
+    combining columns
+    [Administrative_Duration, Informational_Duration, ProductRelated_Duration] --> time spent
+    [BounceRates, ExitRates] --> dislike rates
+    [SpecialDay] --> y_n_special_day >= 0.5 = 1 and < 0.5 = 0
+    [Month] --> semester = [1, 2]
+
+    preprocessing
+    encoded object columns
+    """
+
+    sems_1 = ["Jan", "Feb", "Mar", "Apr", "May", "June"]
+    sems_2 = ["Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    duration_columns = [
+        "Administrative_Duration",
+        "Informational_Duration",
+        "ProductRelated_Duration",
+    ]
+    rates_columns = ["BounceRates", "ExitRates"]
+    new_cat_columns = ["y_n_special_day", "Semester"]
+    obj_columns = X_OBJECT_COLUMNS.copy()
+    obj_columns.extend(new_cat_columns)
+
+    # Loading data
+    df = read_data(DATASET_PATH)
+
+    # Feature Engineering
+    df["TimeSpent"] = df[duration_columns].sum(axis=1)
+    df["DislikeRates"] = df[rates_columns].sum(axis=1)
+    df["y_n_special_day"] = df["SpecialDay"].apply(lambda x: 1 if x >= 0.5 else 0)
+    df["Semester"] = df["Month"].apply(lambda x: 1 if x in sems_1 else 2)
+
+    # Preprocessing
+    encoded_df = encode_data(df, obj_columns)
+
+    df = df.drop(columns=obj_columns)
+
+    df = pd.concat([df, encoded_df], axis=1)
+
+    # Modelling
+    X = df.drop(columns=[Y_COLUMN])
+    y = df[Y_COLUMN]
+
+    X_train, X_test, y_train, y_test = train_test(X, y, MODEL)
+
+    for i in range(len(MODEL)):
+        print(f"\nTitle : {MODEL_TITLE[i]}")
+        plot_classification_report(y_test, MODEL[i].predict(X_test))
+
+    fig, ax = plt.subplots(figsize=(15, 9), nrows=2, ncols=2)
+
+    for i in range(4):
+        y_pred = MODEL[i].predict(X_test)
+        if i < 2:
+            plot_confusion_matrix(y_test, y_pred, ax=ax[0, i])
+
+            ax[0, i].set_title(MODEL_TITLE[i])
+        else:
+            i_ = i - 2
+
+            plot_confusion_matrix(y_test, y_pred, ax=ax[1, i_])
+
+            ax[1, i_].set_title(MODEL_TITLE[i])
--- plotting/plotting_data.py
+++ plotting/plotting_data.py
@@ -3,8 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from cleaning.cleaning_data import encode_data
+
 
 def plot_correlation(df):
+    df = encode_data(df, list(df.columns))
+
     corr = df.corr()
 
     sns.heatmap(round(abs(corr), 2), annot=True)
